483142a8f71d1c090fba4af6136ea53a8209c5c7
napro1299 <blaketadner@gmail.com>
2022-09-21 20:29:38 -0400

    Renamed redundant EpsilonUtils to Utils

--- src/main/java/frc/robot/util/Utils.java
+++ src/main/java/frc/robot/util/Utils.java
@@ -1,6 +1,6 @@
 package frc.robot.util;
 
-public class EpsilonUtil {
+public class Utils {
   public static boolean epsilonEquals(double value, double desiredValue, double epsilon) {
     return Math.abs(desiredValue - value) <= epsilon;
   }
